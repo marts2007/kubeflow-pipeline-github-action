bdbc4f8bd3e7ff5a822a08ff48d21aaad9b908c9
Dmitry <admin.marts@gmail.com>
2022-01-18 16:36:52 +0300

    fix

--- action/config.js
+++ action/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const core = require("./code/actions/core");
 class Config{
     constructor(){
-        this.endpointUrl = core.getInput('kubeflowEndpoint');
+        this.endpointUrl = core.getInput('KUBEFLOWENDPOINT');
         this.pipelineFilePath = core.getInput('pipelineFilePath');
         this.maxFileSizeBytes = (core.getInput('maxFileSizeBytes')!=undefined) ? core.getInput('maxFileSizeBytes') : 32000000
         this.pipelineName =  (core.getInput('pipelineName')!=undefined) ? core.getInput("pipelineName")  : 'newPipeline'
@@ -11,8 +11,8 @@ class Config{
         this.namespace = core.getInput('namespace')
         this.runDescription = core.getInput('runDescription');
         this.runName = core.getInput('runName')
-        this.userid = core.getInput('userid')
-        this.password = core.getInput('password')
+        this.userid = core.getInput('USERID')
+        this.password = core.getInput('PASSWORD')
         if (this.userid==undefined) throw new Error("Please set userid env (api login)")
         if (this.userid==undefined) throw new Error("Please set password env (api password)")
         if (this.namespace==undefined) throw new Error('Please set the namespace env')
